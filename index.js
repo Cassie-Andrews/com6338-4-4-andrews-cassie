15d5673c2416c809727a1ff7bfc08495fba819b4
Cassie Andrews <candrews1@ufl.edu>
2024-09-21 15:05:27 -0400

    pseudocode setup 01

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+
+// do not edit the code above this point!! //
+
+// When the page loads, your code should select a word at random from the provided words array and place it in the #word-to-guess element with its letters replaced with underscores. //
+
+// The game should also display ten remaining guesses in the #remaining-guesses element. //
+
