a4e5ec27651379ae929579e87c15163f5ace1b8f
Cassie Andrews <candrews1@ufl.edu>
2024-09-22 23:56:46 -0400

    restarting from function onkeyup

--- index.js
+++ index.js
@@ -14,11 +14,6 @@ var words = [
 
 // NO EDITS ABOVE THIS POINT //
 
-
-
-
-
-
 /* If the letter is included, it should replace the underscores in the displayed word (displayed in the #word-to-guess element) with the instances of that letter. 
 
 If the letter is not included, the #word-to-guess element should remain unchanged, but the incorrectly-guessed letter should be added to the #incorrect-letters element and the #remaining-guesses element should reflect one fewer remaining guess. */
@@ -42,62 +37,48 @@ var wins = 0;
 var losses = 0;
 
 window.onload = startGame;
-/* When the page loads, your code should select a word at random from the provided words array and place it in the #word-to-guess element with its letters replaced with underscores. */
-
+// When the page loads... 
 
 function startGame() {
-  // pick a random word from the words array
-  wordToGuess = words[Math.floor(Math.random() * words.length)]; // calc # of letters in wordToGuess
+  // your code should select a word at random from the provided words array and place it in the #word-to-guess element with its letters replaced with underscores.
+  wordToGuess = words[Math.floor(Math.random() * words.length)];
   console.log(wordToGuess)
   console.log(wordToGuess.length) 
   remainingGuesses = 10;
   // should display 10 guesses at game start
   incorrectLetters = [];
 
+  // reference elements in HTML doc
   document.getElementById('word-to-guess').textContent = '_'.repeat(wordToGuess.length); // display one _ for each letter
   document.getElementById('previous-word').textContent = '';
   document.getElementById('incorrect-letters').textContent = incorrectLetters;
   document.getElementById('remaining-guesses').textContent = remainingGuesses;
   document.getElementById('wins').textContent = wins;
   document.getElementById('losses').textContent = losses;
-
-  document.addEventListener('keyup', analyzeKeyUp);
+  //do i need an event listener here?;
 }
 
 // only allows letters to be input, case does not matter
 
-function analyzeKeyUp(e) {
-  // access user's keypress
-  var letter = e.key;
-  console.log(letter.toUpperCase())
+document.onkeyup = function(e) {
+  var letters = /^[A-Za-z]+$/;
+  console.log(e.key) // access user's keypress
   
-  // if key pressed is a letter
-  if (!/^[A-Za-z]+$/.test(letter)) {  
- 
-  }
+  // check if key pressed is a letter
   // When the user presses a letter key, your code should check whether the letter is included in the word. 
-    // if key pressed is a letter AND letter is correct
-    if (wordToGuess.includes(letter)) {
-      for (i=0; i < wordToGuess.length; i++) { // loop to compare picked letter with word's letters
-
-        // ** need to show correct letter in wordToGuessDisplay
-        // display remaining guesses
-        //need to display correct letter in the word instead of _
-      }
-    } else {
-        // increment and show incorrect letter
-        (remainingGuesses)-- //-1 guess remgaining
-        remainingGuesses.textContent = remainingGuesses; // display remaining guesses
-        console.log(remainingGuesses)
-        incorrectLetters.textContent = e.key //shows last letter guessed
-        // ** troubleshoot - want to keep all previous incorrect letter guesses on display
-    }
-
-    // show previous word
-    // show number of wins/losses
-    // The game should also display ten remaining guesses in the #remaining-guesses element.
-
-
-    // if key pressed is NOT a letter
+  // if key pressed is a letter AND letter is correct
+  // loop to compare picked letter with word's letters
+  // ** need to show correct letter in wordToGuessDisplay
+  // display remaining guesses
+  //need to display correct letter in the word instead of _
+  // increment and show incorrect letter
+      ////(remainingGuesses)-- //-1 guess remgaining
+      ////remainingGuesses.textContent = remainingGuesses; // display remaining guesses
+      ////console.log(remainingGuesses)
+      ////incorrectLetters.textContent = e.key //shows last letter guessed
+  // show previous word
+  // show number of wins/losses
+  // The game should also display ten remaining guesses in the #remaining-guesses element.
+  // if key pressed is NOT a letter
   }
   
